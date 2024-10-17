807cfac98ad04975c039453f23092f9cece81b3d
Ayush Tripathi <tripathi.at02@gmail.com>
2024-10-17 16:30:24 +0530

    added appsettings.json

--- DAOLibrary/InsuranceServiceImpl.cs
+++ DAOLibrary/InsuranceServiceImpl.cs
@@ -6,39 +6,44 @@ namespace DAOLibrary
 {
     public class InsuranceServiceImpl : IPolicyService
     {
-        private SqlConnection sqlConnection = UtilLibrary.DBConnection.GetConnectionString();
-
-        public InsuranceServiceImpl()
-        {
-            sqlConnection.Open();
-        }
-
         public bool CreatePolicy(Policy policy)
         {
-            using (SqlCommand command = new SqlCommand("INSERT INTO Policies (PolicyNumber) VALUES (@PolicyNumber)", sqlConnection))
+            string connectionString = UtilLibrary.DBConnection.ReturnCn("dbCn");
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString)) 
             {
-                command.Parameters.AddWithValue("@PolicyNumber", policy.PolicyNumber);
-                int rowsAffected = command.ExecuteNonQuery();
-                return rowsAffected > 0;
+                sqlConnection.Open();
+                using (SqlCommand command = new SqlCommand("INSERT INTO Policies (PolicyNumber) VALUES (@PolicyNumber)", sqlConnection))
+                {
+                    command.Parameters.AddWithValue("@PolicyNumber", policy.PolicyNumber);
+                    int rowsAffected = command.ExecuteNonQuery();
+                    return rowsAffected > 0;
+                }
             }
+            
         }
 
         public Policy GetPolicy(int policyId)
         {
-            using (SqlCommand command = new SqlCommand("SELECT PolicyId, PolicyNumber FROM Policies WHERE PolicyId = @PolicyId", sqlConnection))
+            string connectionString = UtilLibrary.DBConnection.ReturnCn("dbCn");
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
             {
-                command.Parameters.AddWithValue("@PolicyId", policyId);
-                using (SqlDataReader reader = command.ExecuteReader())
+                sqlConnection.Open();
+                using (SqlCommand command = new SqlCommand("SELECT PolicyId, PolicyNumber FROM Policies WHERE PolicyId = @PolicyId", sqlConnection))
                 {
-                    if (reader.Read())
+                    command.Parameters.AddWithValue("@PolicyId", policyId);
+                    using (SqlDataReader reader = command.ExecuteReader())
                     {
-                        return new Policy
+                        if (reader.Read())
                         {
-                            PolicyID = (int)reader["PolicyId"],
-                            PolicyNumber = reader["PolicyNumber"].ToString()
-                        };
+                            return new Policy
+                            {
+                                PolicyID = (int)reader["PolicyId"],
+                                PolicyNumber = reader["PolicyNumber"].ToString()
+                            };
+                        }
                     }
                 }
+
             }
 
             throw new PolicyNotFoundException($"Policy with ID {policyId} not found.");
@@ -46,42 +51,58 @@ namespace DAOLibrary
 
         public List<Policy> GetAllPolicies()
         {
-            List<Policy> policies = new List<Policy>();
-            using (SqlCommand command = new SqlCommand("SELECT PolicyId, PolicyNumber FROM Policies", sqlConnection))
+            string connectionString = UtilLibrary.DBConnection.ReturnCn("dbCn");
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
             {
-                using (SqlDataReader reader = command.ExecuteReader())
+                sqlConnection.Open();
+                List<Policy> policies = new List<Policy>();
+                using (SqlCommand command = new SqlCommand("SELECT PolicyId, PolicyNumber FROM Policies", sqlConnection))
                 {
-                    while (reader.Read())
+                    using (SqlDataReader reader = command.ExecuteReader())
                     {
-                        policies.Add(new Policy
+                        while (reader.Read())
                         {
-                            PolicyID = (int)reader["PolicyId"],
-                            PolicyNumber = reader["PolicyNumber"].ToString()
-                        });
+                            policies.Add(new Policy
+                            {
+                                PolicyID = (int)reader["PolicyId"],
+                                PolicyNumber = reader["PolicyNumber"].ToString()
+                            });
+                        }
                     }
                 }
-            }
-            return policies;
+                return policies;
+            } 
         }
 
         public bool UpdatePolicy(Policy policy)
         {
-            using (SqlCommand command = new SqlCommand("UPDATE Policies SET PolicyNumber = @PolicyNumber WHERE PolicyId = @PolicyId", sqlConnection))
+            string connectionString = UtilLibrary.DBConnection.ReturnCn("dbCn");
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString)) 
             {
-                command.Parameters.AddWithValue("@PolicyId", policy.PolicyID);
-                command.Parameters.AddWithValue("@PolicyNumber", policy.PolicyNumber);
-                int rowsAffected = command.ExecuteNonQuery();
-                return rowsAffected > 0;
+                sqlConnection.Open();
+                using (SqlCommand command = new SqlCommand("UPDATE Policies SET PolicyNumber = @PolicyNumber WHERE PolicyId = @PolicyId", sqlConnection))
+                {
+                    command.Parameters.AddWithValue("@PolicyId", policy.PolicyID);
+                    command.Parameters.AddWithValue("@PolicyNumber", policy.PolicyNumber);
+                    int rowsAffected = command.ExecuteNonQuery();
+                    return rowsAffected > 0;
+                }
             }
+            
         }
 
         public bool DeletePolicy(int policyId)
         {
-            using (SqlCommand command = new SqlCommand("DELETE FROM Policies WHERE PolicyId = @PolicyId", sqlConnection))
+            string connectionString = UtilLibrary.DBConnection.ReturnCn("dbCn");
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString)) 
             {
-                command.Parameters.AddWithValue("@PolicyId", policyId);
-                int rowsAffected = command.ExecuteNonQuery();
-                return rowsAffected > 0;
+                sqlConnection.Open();
+                using (SqlCommand command = new SqlCommand("DELETE FROM Policies WHERE PolicyId = @PolicyId", sqlConnection))
+                {
+                    command.Parameters.AddWithValue("@PolicyId", policyId);
+                    int rowsAffected = command.ExecuteNonQuery();
+                    return rowsAffected > 0;
+                }
             }
         }
 
--- UtilLibrary/DBConnection.cs
+++ UtilLibrary/DBConnection.cs
@@ -1,17 +1,28 @@
 ﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
 
 namespace UtilLibrary
 {
     public class DBConnection
     {
-        static string cnstring = @"Data Source=.\sqlexpress;Initial Catalog=InsuranceDB;Integrated Security=True;Trust Server Certificate=True";
-
-        public static SqlConnection GetConnectionString()
+        private static IConfigurationRoot _configuration;
+        static string cnstring = null;
+        static DBConnection()
         {
-            SqlConnection cn = new SqlConnection(cnstring);
-
-            return cn;
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("C:\\Users\\Ayush Tripathi\\OneDrive\\Desktop\\Technical Training\\InsuranceManagementSystem\\UtilLibrary\\appsettings.json", 
+                optional: true, reloadOnChange: true);
+            _configuration = builder.Build();
         }
 
+
+        public static string ReturnCn(string key)
+        {
+            cnstring = _configuration.GetConnectionString(key);
+            return cnstring;
+
+        }
+        
     }
 }
